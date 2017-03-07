ab73c8ea00624f40900f46d6c977c311152469c1
Vanessa R <vanessaroux9@gmail.com>
2017-03-07 16:27:06 +0100

    Revert "coucou"
    This reverts commit de224341c261f2a1c97355614c549bff2b068421.

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/cool', function(request, response) {
+	response.send(cool());
+});
 
 app.get('/', function(request, response) {
-		response.send('banane');
-		
-		});
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 
