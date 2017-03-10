187ebb1be00da1d506f2645c45d384217018328d
Lorena Rangel <rangel.loreen@gmail.com>
2017-03-10 14:38:47 +0100

    changing hard coded port to an environmental variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(3000, () => {  
+const server = app.listen(process.env.PORT, () => {  
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);});
 
 // Now, create HTTP POST route method to handle the command:
